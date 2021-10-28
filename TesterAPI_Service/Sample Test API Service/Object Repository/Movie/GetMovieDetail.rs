def157c5f39c86810919cfe2d2c829b2585fca3e
algokelvin-373 <kelvinht373@gmail.com>
2021-10-28 13:18:21 +0700

    [Katalon - API Testing] Success Test and Validation Data Details Movie

---
+++ TesterAPI_Service/Sample Test API Service/Object Repository/Movie/GetMovieDetail.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>I learn this to test automation.</description>
+   <name>GetMovieDetail</name>
+   <tag></tag>
+   <elementGuidId>9e1e89a2-ba35-4e34-a5ee-8e5d8c16c39e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.1.0</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://api.themoviedb.org/3/movie/580489?api_key=5a9914ea9eb3bfc1addfe13b0b92b0b4&amp;language=en-US</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
