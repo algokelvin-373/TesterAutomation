61c805637b98593a2af1e7d2dfde4dc7762fbefe
algokelvin-373 <kelvinht373@gmail.com>
2024-10-14 20:54:35 +0700

    [Two Number Operator] Create Test Suite Collection

---
+++ KatalonMobileAndroid/TwoNumberOperator/Test Suites/TSC_TwoNumberOperator.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description>Test Suite Collection for APK Two Number Operator</description>
+   <name>TSC_TwoNumberOperator</name>
+   <tag>#twonumberoperator</tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Mobile</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>deviceName</key>
+                  <value>TECNO TECNO KJ5n (Android 13)</value>
+               </entry>
+               <entry>
+                  <key>deviceId</key>
+                  <value>11163693C4003530</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>Android</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/TS_Positive_Scenario</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Mobile</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>deviceName</key>
+                  <value>TECNO TECNO KJ5n (Android 13)</value>
+               </entry>
+               <entry>
+                  <key>deviceId</key>
+                  <value>11163693C4003530</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>Android</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/TS_Negative_Scenario</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
